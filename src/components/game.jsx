d67d807beaf70c5b2292a03bdb5fa0ae5d88edb6
Justin <jdkoeppen@gmail.com>
2018-08-27 23:10:33 -0700

    Redone Initial Commit

---
+++ src/components/game.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  constructor(props) {
+    super(props),
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Game;
